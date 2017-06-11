f09ed39437c0a08930856a7edf3cff359eaaf4ba
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-06-11 22:28:28 +0900

    no message

---
+++ src/deferred_acceptance2.jl
@@ -0,0 +1,45 @@
+function deferred_acceptance(m_prefs::Vector{Vector{Int}},
+                             f_prefs::Vector{Vector{Int}})
+    return deferred_acceptance(m_prefs_2d, f_prefs_2d)
+end
+
+function deferred_acceptance(m_prefs,f_prefs)
+    m_size = size(m_prefs,2)
+    f_size = size(f_prefs,2)
+    m_matched = zeros(Int64,m_size)
+    f_matched = zeros(Int64,f_size)
+    m_prp = ones(Int64,m_size)
+    for j in 1:f_size
+        for i in 1:m_size
+            if m_matched[i] == 0 && m_prp[i] < (f_size+2)
+                if m_prefs[m_prp[i],i] == 0
+                    m_matched[i] = 0
+                    m_prp[i] = f_size+2
+                else
+                    if f_matched[m_prefs[m_prp[i],i]] == 0
+                        if findfirst(f_prefs[:,m_prefs[m_prp[i],i]],0) < findfirst(f_prefs[:,m_prefs[m_prp[i],i]],i)
+                            f_matched[m_prefs[m_prp[i],i]] = 0
+                            m_matched[i] = 0
+                            m_prp[i]+=1
+                        else
+                            f_matched[m_prefs[m_prp[i],i]] = i
+                            m_matched[i] = m_prefs[m_prp[i],i]
+                            m_prp[i]+=1
+                        end
+                    else
+                        if findfirst(f_prefs[:,m_prefs[m_prp[i],i]],f_matched[m_prefs[m_prp[i],i]]) < findfirst(f_prefs[:,m_prefs[m_prp[i],i]],i)
+                            m_matched[i] = 0
+                            m_prp[i]+=1
+                        else
+                            m_matched[f_matched[m_prefs[m_prp[i],i]]] = 0
+                            m_matched[i] = m_prefs[m_prp[i],i]
+                            f_matched[m_prefs[m_prp[i],i]] = i
+                            m_prp[i]+=1
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return(m_matched,f_matched)
+end
