161ca30b631ba70fbec59332b465701c410ea01c
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-06-15 21:42:35 +0900

    関数を対応させた

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -2,39 +2,37 @@ module MyMatching
 
 export deferred_acceptance
 
-function deferred_acceptance(m_prefs,f_prefs)
-    m_size = size(m_prefs,2)
-    f_size = size(f_prefs,2)
+function deferred_acceptance(m_prefs::Vector{Vector{Int}},f_prefs::Vector{Vector{Int}})
+    m_size = length(m_prefs)
+    f_size = length(f_prefs)
     m_matched = zeros(Int64,m_size)
     f_matched = zeros(Int64,f_size)
     m_prp = ones(Int64,m_size)
     for j in 1:f_size
         for i in 1:m_size
-            if m_matched[i] == 0 && m_prp[i] < (f_size+2)
-                if m_prefs[m_prp[i],i] == 0
-                    m_matched[i] = 0
-                    m_prp[i] = f_size+2
-                else
-                    if f_matched[m_prefs[m_prp[i],i]] == 0
-                        if findfirst(f_prefs[:,m_prefs[m_prp[i],i]],0) < findfirst(f_prefs[:,m_prefs[m_prp[i],i]],i)
-                            f_matched[m_prefs[m_prp[i],i]] = 0
-                            m_matched[i] = 0
-                            m_prp[i]+=1
-                        else
-                            f_matched[m_prefs[m_prp[i],i]] = i
-                            m_matched[i] = m_prefs[m_prp[i],i]
-                            m_prp[i]+=1
-                        end
+            if m_prp[i] > length(m_prefs[i])
+                m_matched[i] = 0
+            end
+            if m_matched[i] == 0 && m_prp[i] <= length(m_prefs[i])
+                if f_matched[m_prefs[i][m_prp[i]]] == 0
+                    if findfirst(f_prefs[m_prefs[i]][m_prp[i]], i) == 0
+                        f_matched[m_prefs[i][m_prp[i]]] = 0
+                        m_matched[i] = 0
+                        m_prp[i]+=1
                     else
-                        if findfirst(f_prefs[:,m_prefs[m_prp[i],i]],f_matched[m_prefs[m_prp[i],i]]) < findfirst(f_prefs[:,m_prefs[m_prp[i],i]],i)
-                            m_matched[i] = 0
-                            m_prp[i]+=1
-                        else
-                            m_matched[f_matched[m_prefs[m_prp[i],i]]] = 0
-                            m_matched[i] = m_prefs[m_prp[i],i]
-                            f_matched[m_prefs[m_prp[i],i]] = i
-                            m_prp[i]+=1
-                        end
+                        f_matched[m_prefs[i][m_prp[i]]] = i
+                        m_matched[i] = m_prefs[i][m_prp[i]]
+                        m_prp[i]+=1
+                    end
+                else
+                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], f_matched[m_prefs[i]][m_prp[i]]) < findfirst(f_prefs[m_prefs[i][m_prp[i]]], i)
+                        m_matched[i] = 0
+                        m_prp[i]+=1
+                    else
+                        m_matched[f_matched[m_prefs[i]][m_prp[i]]] = 0
+                        m_matched[i] = m_prefs[i][m_prp[i]]
+                        f_matched[m_prefs[i][m_prp[i]]] = i
+                        m_prp[i]+=1
                     end
                 end
             end
