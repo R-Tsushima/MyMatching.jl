f42965e2ad0e89fd38a922edbc0cddd90ea37c3c
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-06-17 15:44:15 +0900

    関数修正

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -24,10 +24,7 @@ function deferred_acceptance(m_prefs::Vector{Vector{Int}},f_prefs::Vector{Vector
                         m_prp[i]+=1
                     end
                 else
-                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) == 0
-                        m_matched[i] = 0
-                        m_prp[i]+=1
-                    elseif findfirst(f_prefs[m_prefs[i][m_prp[i]]], f_matched[m_prefs[i]][m_prp[i]]) < findfirst(f_prefs[m_prefs[i][m_prp[i]]], i)
+                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) == 0 || findfirst(f_prefs[m_prefs[i][m_prp[i]]], f_matched[m_prefs[i][m_prp[i]]]) < findfirst(f_prefs[m_prefs[i][m_prp[i]]], i)
                         m_matched[i] = 0
                         m_prp[i]+=1
                     else
