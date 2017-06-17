08496da339e87275769b3eb2a13b3515f5a1adf9
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-06-17 15:51:27 +0900

    関数修正

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -28,7 +28,7 @@ function deferred_acceptance(m_prefs::Vector{Vector{Int}},f_prefs::Vector{Vector
                         m_matched[i] = 0
                         m_prp[i]+=1
                     else
-                        m_matched[f_matched[m_prefs[i]][m_prp[i]]] = 0
+                        m_matched[f_matched[m_prefs[i][m_prp[i]]]] = 0
                         m_matched[i] = m_prefs[i][m_prp[i]]
                         f_matched[m_prefs[i][m_prp[i]]] = i
                         m_prp[i]+=1
