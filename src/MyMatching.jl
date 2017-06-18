4b46868a39ea56e3810938691a166a4078c23d9d
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-06-18 22:48:16 +0900

    関数修正

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -10,30 +10,20 @@ function deferred_acceptance(m_prefs::Vector{Vector{Int}},f_prefs::Vector{Vector
     m_prp = ones(Int64,m_size)
     for j in 1:f_size
         for i in 1:m_size
-            if m_prefs[i][m_prp[i]] == 0
-                m_matched[i] = 0
-            elseif m_matched[i] == 0 && m_prp[i] <= length(m_prefs[i])
+            if m_matched[i] == 0 && m_prp[i] <= length(m_prefs[i])
                 if f_matched[m_prefs[i][m_prp[i]]] == 0
-                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) == 0
-                        f_matched[m_prefs[i][m_prp[i]]] = 0
-                        m_matched[i] = 0
-                        m_prp[i]+=1
-                    else
+                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) != 0
                         f_matched[m_prefs[i][m_prp[i]]] = i
-                        m_matched[i] = m_prefs[i][m_prp[i]]
-                        m_prp[i]+=1
+                        m_matched[i] = m_prefs[i][m_prp[i]
                     end
                 else
-                    if findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) == 0 || findfirst(f_prefs[m_prefs[i][m_prp[i]]], f_matched[m_prefs[i][m_prp[i]]]) < findfirst(f_prefs[m_prefs[i][m_prp[i]]], i)
-                        m_matched[i] = 0
-                        m_prp[i]+=1
-                    else
+                    if 0 < findfirst(f_prefs[m_prefs[i][m_prp[i]]], i) < findfirst(f_prefs[m_prefs[i][m_prp[i]]], f_matched[m_prefs[i][m_prp[i]]])
                         m_matched[f_matched[m_prefs[i][m_prp[i]]]] = 0
                         m_matched[i] = m_prefs[i][m_prp[i]]
                         f_matched[m_prefs[i][m_prp[i]]] = i
-                        m_prp[i]+=1
                     end
                 end
+                m_prp[i]+=1
             end
         end
     end
