4f0b7df334d9aa77f8f31187960362a3f9e65f64
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-07-01 21:09:20 +0900

    多対一対応コード追加

--- src/deferred_acceptance2.jl
+++ src/deferred_acceptance2.jl
@@ -1,3 +1,56 @@
+#多対一のケース
+function deferred_acceptance(prop_prefs::Vector{Vector{Int}},
+                             resp_prefs::Vector{Vector{Int}},
+                             caps::Vector{Int})
+    m = length(prop_prefs)
+    n = length(resp_prefs)
+    prop_matched = zeros(Int64,m)
+    resp_matched = zeros(Int64,sum(caps))
+    count = ones(Int64,m)
+    accept = zeros(Int64,n)
+    indptr = Array{Int}(n+1)
+    indptr[1] = 1
+    for i in 1:n
+        indptr[i+1] = indptr[i] + caps[i]
+    end
+
+    for j in 1:n
+        for i in 1:m
+            k = prop_prefs[i][count[i]]
+            if prop_matched[i] == 0 && count[i] <= length(prop_prefs[i])
+                if findfirst(resp_prefs[k], i) != 0
+                    if accept[k] < caps[k]
+                        resp_matched[indptr[k+1]-1 - accept[k]] = i
+                        prop_matched[i] = k
+                        accept[k]+=1
+                    else
+                        list = resp_matched[indptr[k]:indptr[k+1]-1]
+                        for l in 1:caps[k]
+                            rank[l] = findfirst(resp_prefs[prop_prefs[i][count[i]]], list[l])
+                        end
+                        if 0 < findfirst(resp_prefs[prop_prefs[i][count[i]]], i) < maximum(rank)
+                            resp_matched[indptr[k] + indmax(rank) - 1]
+                            prop_matched[list[indmax(rank)]] = 0
+                            prop_matched[i] = k
+                        end
+                    end
+                end
+                count[i]+=1
+            end
+        end
+    end
+    return prop_matched, resp_matched, indptr
+end
+
+#一対一のケース
+function deferred_acceptance(prop_prefs::Vector{Vector{Int}},
+                                resp_prefs::Vector{Vector{Int}})
+    caps = ones(Int, length(resp_prefs))
+    prop_matches, resp_matches, indptr =
+        deferred_acceptance(prop_prefs, resp_prefs, caps)
+    return prop_matches, resp_matches
+end
+
 function deferred_acceptance(m_prefs::Vector{Vector{Int}},f_prefs::Vector{Vector{Int}})
     m_size = length(m_prefs)
     f_size = length(f_prefs)
